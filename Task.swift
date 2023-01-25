d34296a88bfc86aee6bc99debcbf9499ca96b198
Yuki Naka <48172941+Yuki-Naka-Sharing-Study@users.noreply.github.com>
2023-01-25 22:44:15 +0900

    【まとめ】完了。【課題】の直前。

---
+++ Task.swift
@@ -0,0 +1,27 @@
+//
+//  Task.swift
+//  taskapp
+//
+//  Created by 仲優樹 on 2023/01/25.
+//
+
+import RealmSwift
+
+class Task: Object {
+    // 管理用 ID。プライマリーキー
+    @objc dynamic var id = 0
+
+    // タイトル
+    @objc dynamic var title = ""
+
+    // 内容
+    @objc dynamic var contents = ""
+
+    // 日時
+    @objc dynamic var date = Date()
+
+    // id をプライマリーキーとして設定
+    override static func primaryKey() -> String? {
+        return "id"
+    }
+}
