f8e76d14ec46770b2cc8eca73f44598b20b376a2
Yuki Naka <48172941+Yuki-Naka-Sharing-Study@users.noreply.github.com>
2023-01-29 12:15:32 +0900

    「Category」関連のコード追加。

--- taskapp/InputViewController.swift
+++ taskapp/InputViewController.swift
@@ -28,6 +28,7 @@ class InputViewController: UIViewController {
         self.view.addGestureRecognizer(tapGesture)
 
         titleTextField.text = task.title
+        categoryTextField.text = task.category
         contentsTextView.text = task.contents
         datePicker.date = task.date
     }
@@ -35,6 +36,7 @@ class InputViewController: UIViewController {
     override func viewWillDisappear(_ animated: Bool) {
         try! realm.write {
             self.task.title = self.titleTextField.text!
+            self.task.category = self.categoryTextField.text!
             self.task.contents = self.contentsTextView.text
             self.task.date = self.datePicker.date
             self.realm.add(self.task, update: .modified)
@@ -54,11 +56,19 @@ class InputViewController: UIViewController {
        } else {
            content.title = task.title
        }
+        
+       if task.category == "" {
+           content.categoryIdentifier = "(カテゴリなし)"
+       } else {
+           content.categoryIdentifier = task.category
+       }
+        
        if task.contents == "" {
            content.body = "(内容なし)"
        } else {
            content.body = task.contents
        }
+        
        content.sound = UNNotificationSound.default
 
        // ローカル通知が発動するtrigger（日付マッチ）を作成
